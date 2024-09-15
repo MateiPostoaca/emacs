0c8a948877792bd2c8443400b853fa5412bbe3f5
MateiPostoaca <postoacamatei@gmail.com>
2024-09-15 18:42:22 +0300

    Changed org-agenda directory

--- init.el
+++ init.el
@@ -170,7 +170,7 @@
   :hook (org-mode . efs/org-mode-visual-fill))
 
 (setq org-directory "/mnt/d/Documents/Personal/Notes")
-(setq org-agenda-files (directory-files-recursively org-directory "\\.org$"))
+(setq org-agenda-files (directory-files-recursively (concat org-directory "/Struct/Plans") "\\.org$"))
 
 (setq org-agenda-start-with-log-mode t)
 (setq org-log-done 'time)
